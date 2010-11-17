d283bc81c3c89afd00f3c65a8e68df0b44529f08
Kevin Fitzpatrick <kfitzpatrick@Kevin-Fitzpatricks-MacBook-Pro.local>
2010-11-16 22:42:44 -0800

    Map root to foos controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 ActionController::Routing::Routes.draw do |map|
   map.resources :foos
+  
+  map.root :controller => "foos"
 
   # The priority is based upon order of creation: first created -> highest priority.
 
